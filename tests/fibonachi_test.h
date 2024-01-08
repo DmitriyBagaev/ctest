9e71b7953842a86dcf5006ef4cccb0b19fecd5ec
dmitriybagaev <d.p.bagaev@gmail.com>
2024-01-09 00:53:09 +0300

    Test

--- tests/fibonachi_test.h
+++ tests/fibonachi_test.h
@@ -53,3 +53,24 @@ TEST(fibonachiTest, inputFile) {
 }
 
 #endif // FIBONACHI_H
+
+
+#ifndef SQUARES_H
+#define SQUARES_H
+
+#include <gtest/gtest.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+extern "C" {
+#include "myfunc.h"
+}
+
+TEST(squaresTest, num0) {
+ASSERT_EQ(squares(1.0), 20.0, 2.0);
+}
+
+
+#endif // SQUARES_H
