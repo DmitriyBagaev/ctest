5055427a6d99c20215b55f0970e84dc405327469
dmitriybagaev <d.p.bagaev@gmail.com>
2024-01-09 01:30:21 +0300

    Test

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,8 +5,8 @@
 
 #include <gtest/gtest.h>
 
-int main(int argc, char *argv[], int argc)
+int main(int argc, char *argv[])
 {
-    ::testing::InitGoogleTest(&argc, argv, argc);
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
