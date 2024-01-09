fce06201b8e6bfb4c7e7aaf61299a2e10f82f158
dmitriybagaev <d.p.bagaev@gmail.com>
2024-01-09 19:10:40 +0300

    Pointer method

--- tests/squares_test.h
+++ tests/squares_test.h
@@ -11,17 +11,26 @@ extern "C" {
 #include "myfunc.h"
 }
 
+extern int squares(double a, double b, double c, double *root1, double *root2);
+
 TEST(squaresTest, OneRoot) {
-    ASSERT_EQ(squares(1), 1);
+result = squares(1, -2, 1, &root1, &root2);
+ASSERT_EQ(result, 1);
 }
+
 TEST(squaresTest, TwoRoots) {
-    ASSERT_EQ(squares(2), 2);
+result = squares(1, -3, 2, &root1, &root2);
+ASSERT_EQ(result, 2);
 }
+
 TEST(squaresTest, NoRoots) {
-    ASSERT_EQ(squares(0), 0);
+result = squares(1, 0, 1, &root1, &root2);
+ASSERT_EQ(result, 0);
 }
+
 TEST(squaresTest, NotSquare) {
-ASSERT_EQ(squares(-1), -1);
+result = squares(0, 2, 1, &root1, &root2);
+ASSERT_EQ(result, -1);
 }
 
 #endif // SQUARES_H
