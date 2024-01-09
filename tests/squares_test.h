0768b4fe945a3abf0ed7ccc060e4eef7aae8e5b9
dmitriybagaev <d.p.bagaev@gmail.com>
2024-01-09 19:36:39 +0300

    Pointer method

--- tests/squares_test.h
+++ tests/squares_test.h
@@ -14,26 +14,33 @@ extern "C" {
 extern int squares(double a, double b, double c, double *root1, double *root2);
 
 class squaresTest : public ::testing::Test {:
+protected:
     double root1, root2;
     int result;
+
+    void SetUp() override {
+        root1 = 0;
+        root2 = 0;
+        result = 0;
+    }
 };
 
-TEST(squaresTest, OneRoot) {
+TEST_F(squaresTest, OneRoot) {
 result = squares(1, -2, 1, &root1, &root2);
 ASSERT_EQ(result, 1);
 }
 
-TEST(squaresTest, TwoRoots) {
+TEST_F(squaresTest, TwoRoots) {
 result = squares(1, -3, 2, &root1, &root2);
 ASSERT_EQ(result, 2);
 }
 
-TEST(squaresTest, NoRoots) {
+TEST_F(squaresTest, NoRoots) {
 result = squares(1, 0, 1, &root1, &root2);
 ASSERT_EQ(result, 0);
 }
 
-TEST(squaresTest, NotSquare) {
+TEST_F(squaresTest, NotSquare) {
 result = squares(0, 2, 1, &root1, &root2);
 ASSERT_EQ(result, -1);
 }
