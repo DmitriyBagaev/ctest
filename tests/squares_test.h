4bf3a2cffc49220db1a86ee03411f50e90d4b3fe
dmitriybagaev <d.p.bagaev@gmail.com>
2024-01-10 10:53:27 +0300

    Pointer method

--- tests/squares_test.h
+++ tests/squares_test.h
@@ -20,26 +20,26 @@ protected:
 };
 
 TEST_F(squaresTest, OneRoot) {
-result = squares(1, -2, 1, &root1, &root2);
-ASSERT_EQ(result, 1);
-ASSERT_EQ(1 * root1 * root1 - 2 * root1 + 1, 0, 0)
+    result = squares(1, -2, 1, &root1, &root2);
+    ASSERT_EQ(result, 1);
+    ASSERT_NEAR(1 * root1 * root1 - 2 * root1 + 1, 0, 1e-6);
 }
 
 TEST_F(squaresTest, TwoRoots) {
-result = squares(1, -3, 2, &root1, &root2);
-ASSERT_EQ(result, 2);
-ASSERT_EQ(1 * root1 * root1 - 3 * root1 + 1, 0, 0)
-ASSERT_EQ(1 * root2 * root2 - 3 * root2 + 1, 0, 0)
+    result = squares(1, -3, 2, &root1, &root2);
+    ASSERT_EQ(result, 2);
+    ASSERT_NEAR(1 * root1 * root1 - 3 * root1 + 2, 0, 1e-6);
+    ASSERT_NEAR(1 * root2 * root2 - 3 * root2 + 2, 0, 1e-6);
 }
 
 TEST_F(squaresTest, NoRoots) {
-result = squares(1, 0, 1, &root1, &root2);
-ASSERT_EQ(result, 0);
+    result = squares(1, 0, 1, &root1, &root2);
+    ASSERT_EQ(result, 0);
 }
 
 TEST_F(squaresTest, NotSquare) {
-result = squares(0, 2, 1, &root1, &root2);
-ASSERT_EQ(result, -1);
+    result = squares(0, 2, 1, &root1, &root2);
+    ASSERT_EQ(result, -1);
 }
 
 #endif // SQUARES_H
